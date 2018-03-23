52c0dcd74ea557f38fa74f4bab4eabc5c54fd6a3
DeepCFoxXx <learncode23@gmail.com>
2018-03-23 16:37:29 +0000

    wizard name passing

---
+++ specs/wizard_spec.rb
@@ -0,0 +1,18 @@
+require('minitest/autorun')
+require_relative('../wizard.rb')
+require_relative('../wand.rb')
+
+class TestWizard < MiniTest::Test
+
+  def setup
+    # @broken_wand = Wand.new("Oak", "Unicorn Hair")
+    # @elder_wand = Wand.new("Holly", "phoenix Feather")
+    @ron = Wizard.new("Ron Weasley", @broken_wand)
+    @harry = Wizard.new("Harry Potter", @elder_wand)
+  end
+
+  def test_wizard_has_name
+    assert_equal("Ron Weasley", @ron.name)
+  end
+
+end
---
+++ wizard.rb
@@ -0,0 +1,10 @@
+class Wizard
+
+  attr_reader :name
+
+  def initialize(name, wand)
+    @name = name
+    @wand = wand
+  end
+
+end
